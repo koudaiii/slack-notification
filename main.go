4df552acf52c377293533dad2a1a9ac3448ec4e7
koudaiii <cs006061@gmail.com>
2016-09-26 21:09:19 +0900

    only post message to slack

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	// input send text
+	args := strings.Join(os.Args[1:], " ")
+	if args == "" {
+		fmt.Println("no text")
+		os.Exit(0)
+	}
+
+	//Incoming Webhook URL
+	config, err := LoadConfig()
+	if err != nil {
+		fmt.Println("%v", err)
+		os.Exit(1)
+	}
+
+	//Form JSON payload to send to Slack
+	json := `{"text": "` + args + `"}`
+	//Post JSON payload to the Webhook URL
+	client := http.Client{}
+	req, err := http.NewRequest("POST", config.SlackWebhookURL, bytes.NewBufferString(json))
+	req.Header.Set("Content-Type", "application/json")
+	_, err = client.Do(req)
+	if err != nil {
+		fmt.Println("Unable to reach the server.")
+	}
+	defer req.Body.Close()
+}
